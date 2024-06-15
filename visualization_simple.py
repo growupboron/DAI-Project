0e521884d21fb35964afef40698590c16b2aab30
Shankho Boron Ghosh <shankhoghosh123@gmail.com>
2024-06-15 22:12:20 +0200

    Modified Visualization to randominit
    Signed-off-by: Shankho Boron Ghosh <shankhoghosh123@gmail.com>

--- visualization_simple.py
+++ visualization_simple.py
@@ -1,33 +1,49 @@
 from election_simple import BullyElection, RingElection
 import matplotlib.pyplot as plt
+import random
 
-def run_experiments(max_processes):
+def run_experiments(max_processes, random_init, seed, increment):
+    random.seed(seed)
     bully_results = []
     ring_results = []
-    
-    for num_processes in range(1, max_processes + 1):
-        for start_process in range(1, num_processes + 1):
+    for num_processes in range(1, max_processes + 1, increment):
+        if random_init:
+            start_process = random.randint(1, num_processes)
             print(f"\nNumber of Processes: {num_processes}, Initiating Process: {start_process}")
             bully_messages = BullyElection(num_processes, start_process)
             ring_messages = RingElection(num_processes, start_process)
-            
             bully_results.append((num_processes, start_process, bully_messages))
             ring_results.append((num_processes, start_process, ring_messages))
-            
             print(f"Processes: {num_processes}, Initiating Process: {start_process}, Bully Messages: {bully_messages}, Ring Messages: {ring_messages}")
-    
+        else:
+            for start_process in range(1, num_processes + 1):
+                print(f"\nNumber of Processes: {num_processes}, Initiating Process: {start_process}")
+                bully_messages = BullyElection(num_processes, start_process)
+                ring_messages = RingElection(num_processes, start_process)
+                bully_results.append((num_processes, start_process, bully_messages))
+                ring_results.append((num_processes, start_process, ring_messages))
+                print(f"Processes: {num_processes}, Initiating Process: {start_process}, Bully Messages: {bully_messages}, Ring Messages: {ring_messages}")
     return bully_results, ring_results
 
-def plot_line(results1, results2, title1, title2):
+def plot_line(results1, results2, title1, title2, random_init):
     fig, ax = plt.subplots()
-    max_processes = max(max(result[0] for result in results1), max(result[0] for result in results2))
-    for i in range(1, max_processes + 1):
-        xs1 = [result[0] for result in results1 if result[1] == i]
-        ys1 = [result[2] for result in results1 if result[1] == i]
-        ax.plot(xs1, ys1, label=f'{title1} - Initiating Process {i}')
-        xs2 = [result[0] for result in results2 if result[1] == i]
-        ys2 = [result[2] for result in results2 if result[1] == i]
-        ax.plot(xs2, ys2, label=f'{title2} - Initiating Process {i}', linestyle='--')
+    if random_init:
+        xs1 = [result[0] for result in results1]
+        ys1 = [result[2] for result in results1]
+        ax.plot(xs1, ys1, label=title1)
+        xs2 = [result[0] for result in results2]
+        ys2 = [result[2] for result in results2]
+        ax.plot(xs2, ys2, label=title2, linestyle='--')
+        ax.set_title('Random Initiation')
+    else:
+        max_processes = max(max(result[0] for result in results1), max(result[0] for result in results2))
+        for i in range(1, max_processes + 1):
+            xs1 = [result[0] for result in results1 if result[1] == i]
+            ys1 = [result[2] for result in results1 if result[1] == i]
+            ax.plot(xs1, ys1, label=f'{title1} - Initiating Process {i}')
+            xs2 = [result[0] for result in results2 if result[1] == i]
+            ys2 = [result[2] for result in results2 if result[1] == i]
+            ax.plot(xs2, ys2, label=f'{title2} - Initiating Process {i}', linestyle='--')
     ax.set_xlabel('Number of Processes')
     ax.set_ylabel('Number of Messages')
     ax.legend()
@@ -36,7 +52,6 @@ def plot_line(results1, results2, title1, title2):
 def plot_line_high_low(results1, results2, title1, title2):
     fig, axs = plt.subplots(2)
     max_processes = max(max(result[0] for result in results1), max(result[0] for result in results2))
-    
     # Plot for Lowest Process initiation
     xs1_min = [result[0] for result in results1 if result[1] == 1]
     ys1_min = [result[2] for result in results1 if result[1] == 1]
@@ -47,7 +62,6 @@ def plot_line_high_low(results1, results2, title1, title2):
     axs[0].set_xlabel('Number of Processes')
     axs[0].set_ylabel('Number of Messages')
     axs[0].legend()
-
     # Plot for Highest Process initiation
     xs1_max = [result[0] for result in results1 if result[1] == result[0]]
     ys1_max = [result[2] for result in results1 if result[1] == result[0]]
@@ -63,6 +77,16 @@ def plot_line_high_low(results1, results2, title1, title2):
 
 if __name__ == "__main__":
     max_processes = int(input("Enter the max number of processes: "))
-    bully_results, ring_results = run_experiments(max_processes)
-    plot_line(bully_results, ring_results, 'Bully Algorithm', 'Ring Algorithm')
-    plot_line_high_low(bully_results, ring_results, 'Bully Algorithm', 'Ring Algorithm')
+    random_init = input("Do you want random initiation? (yes/no): ").lower() == "yes"
+    seed = None
+    increment = None
+    if random_init:
+        seed = int(input("Enter a seed for the random number generator: "))
+        increment = int(input("Increment step for the experiments: "))
+        bully_results, ring_results = run_experiments(max_processes, random_init, seed, increment)
+        plot_line(bully_results, ring_results, 'Bully Algorithm', 'Ring Algorithm', random_init)
+    else:
+        print(f"\nInitiating all processes in order")
+        bully_results, ring_results = run_experiments(max_processes, random_init, seed, increment)
+        plot_line(bully_results, ring_results, 'Bully Algorithm', 'Ring Algorithm', random_init)
+        plot_line_high_low(bully_results, ring_results, 'Bully Algorithm', 'Ring Algorithm')
